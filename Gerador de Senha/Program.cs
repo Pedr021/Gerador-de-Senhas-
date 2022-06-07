c30d60394fba9dd2f5ec918d69d9bb7b92c8fbf8
Pedro Canuto <104801029+Pedr021@users.noreply.github.com>
2022-06-07 20:53:05 -0300

    Add files via upload

---
+++ Gerador de Senha/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text;
+
+
+
+
+
+namespace GeradordeSenha
+{
+    class Program
+    {
+        static void Main ( string[] args)
+        {
+            int qtd = 15;
+
+            string caracteresPossiveis =
+            "ABCDEFGHIJKLMNOPQRSTUVWXYZÇ" +
+            "ABCDEFGHIJKLMNOPQRSTUVWXYZÇ".ToLower() +
+            "0123456789" + "%$#@!";
+
+            Random sorteio = new Random();
+            int numeroSorteado = 0;
+            StringBuilder password = new StringBuilder();
+           
+            for (int t = 0;  t < qtd; t++)
+            {
+                numeroSorteado = sorteio.Next(0, caracteresPossiveis.Length - 1);
+                password.Append(caracteresPossiveis[numeroSorteado]);
+            }
+
+            Console.Write(password);
+            
+        }
+
+
+    }
+}
